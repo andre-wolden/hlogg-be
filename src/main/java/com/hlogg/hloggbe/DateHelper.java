2dda4f78046a36bbe2bb179eea2147fa26e23bf1
André Wolden <andre.wolden@bekk.no>
2018-11-06 11:29:16 +0100

    lager datehelper som gir alle datoer mellom to spesifiserte datoer

---
+++ src/main/java/com/hlogg/hloggbe/DateHelper.java
@@ -0,0 +1,18 @@
+package com.hlogg.hloggbe;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class DateHelper {
+
+    public static List<LocalDate> getDatesBetweenUsingJava8(LocalDate startDate, LocalDate endDate) {
+        long numOfDaysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+        return IntStream.iterate(0, i -> i + 1)
+                .limit(numOfDaysBetween)
+                .mapToObj(i -> startDate.plusDays(i))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/hlogg/hloggbe/DateResource.java
@@ -0,0 +1,27 @@
+package com.hlogg.hloggbe;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class DateResource {
+
+    @CrossOrigin
+    @RequestMapping(value = "/dates", method = RequestMethod.GET)
+    public List<LocalDate> getDates(){
+
+        LocalDate startDate = LocalDate.of(2018, 01, 01);
+        LocalDate endDate = LocalDate.now();
+
+        List<LocalDate> datesBetween = DateHelper.getDatesBetweenUsingJava8(startDate, endDate);
+
+
+        return datesBetween;
+    }
+
+}
